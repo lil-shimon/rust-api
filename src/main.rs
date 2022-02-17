c6723aa5a6e1d79daf6072b7312cef2432320e4b
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-02-17 23:16:20 +0900

    feat: 🎸 post, get route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod todo;
+
 use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/{id}/{name}/index.html")]
@@ -7,8 +9,8 @@ async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responde
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
+    HttpServer::new(|| App::new().service(todo::get_todo).service(todo::post_todo))
+        .bind("127.0.0.1:8000")?
         .run()
         .await
 }
